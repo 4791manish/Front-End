5d69d9e49f5073b6a93300208d5fba72c7239265
manish <tmk97601@gmail.com>
2016-04-06 13:49:49 +0530

    Initial Setup

---
+++ jsTestCase1.js
@@ -0,0 +1,7 @@
+/* 
+File Name: JsTestCase1
+Project: Front-End Code Practice
+Description: Program To Print 1-10 using for, while, do..while, foreach, for..in loops
+Author: Manish Singh
+*/
+var a;
