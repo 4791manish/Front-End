906baca765193fc661b590c43d8b814828425ea4
manish <tmk97601@gmail.com>
2016-04-07 12:11:21 +0530

    Comitting to update

--- jsTestCase1.js
+++ jsTestCase1.js
@@ -4,4 +4,12 @@ Project: Front-End Code Practice
 Description: Program To Print 1-10 using for, while, do..while, foreach, for..in loops
 Author: Manish Singh
 */
-var a;
+var a=prompt("enter first number");
+var b=prompt("enter second number");
+x=parseInt(x);
+y=parseInt(y);
+document.getElementById("plus").innerHTML ="ADDITION="+(x+y);
+document.getElementById("minus").innerHTML ="SUBSTRACTION="+(x-y);
+document.getElementById("sub").innerHTML ="MULTIPLICATION="+(x*y);
+document.getElementById("div").innerHTML ="DIVISION"+(x/y);
+
