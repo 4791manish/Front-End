d156691ff243454e0b4a8c3e2bc0735d72725b97
manish <tmk97601@gmail.com>
2016-04-07 15:51:02 +0530

    final

--- js/mkJsTestCase2.js
+++ js/mkJsTestCase2.js
@@ -4,8 +4,23 @@ Project: Front-End Code Practice
 Description: Program To Print 1-10 using for, while, do..while, foreach, for..in loops
 Author: Manish Singh
 */
-
+/*
 for(var i=0; i<=10; i++)
 {
-	document.write(i + "<br/>");
+	document.write("<p>"+i+"</p>");
 }
+*/
+/*
+var a=prompt("enter any value");
+for(var i=1; i<=a; i++)
+{
+	document.write(i+" ");
+
+}*/
+/*
+var a=prompt("enter any value");
+var b=prompt("enter another value");
+for(a; a<b; a++){
+	document.write(a+" ");
+}
+*/
