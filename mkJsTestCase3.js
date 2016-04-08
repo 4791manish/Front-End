52e04475ef134d18904ba5edca64a4a33cb1b1d1
manish <tmk97601@gmail.com>
2016-04-08 11:49:41 +0530

    concatinate a string

--- mkJsTestCase3.js
+++ mkJsTestCase3.js
@@ -1,10 +1,15 @@
-document.write("<img src=img/'download.jpg'>");
-/*document.write("<img id='image' src="download.jpg">)*/
-var arr=["p1.png","p2.jpg","p3.jpg"];
-for(i=0; i<=3 ;i++)
-{
-	document.write("<img src='/img"+arr[i]+"'>");
+/* 
+File Name: JsTestCase2
+Project: Front-End Code Practice
+Description: write a program to concatinate to add and display to string inputted by the user through two input satements.
+Author: Manish Singh
+*/
+document.querySelector("#submit").addEventListener("click", MyFunc);
+function MyFunc(){
+	var str1=document.getElementById("str1").value;
+	var str2=document.getElementById("str2").value;
+	var add=str1+str2;
+	document.getElementById("display").innerHTML= add;
 }
- 
 
 
